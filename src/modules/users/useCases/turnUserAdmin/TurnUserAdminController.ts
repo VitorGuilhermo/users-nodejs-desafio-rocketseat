33195c93f7f21d90bc97e874fae0463334646a77
Vitor Guilhermo <viguilhermo@hotmail.com>
2021-12-26 16:22:37 -0300

    challenge done

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,16 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    const id = user_id.toString();
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id: id });
+
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
